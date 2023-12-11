103b2ff0a6ee122a158d67014dd35009a1fe2c4c
mehrdad <jafari.ceai@gmail.com>
2023-12-12 01:29:07 +0330

    add metadata

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -44,12 +44,13 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "failed to create refresh token: %s", err)
 	}
 
+	m := server.extractMetadata(ctx)
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
 		ID:           refreshPayload.ID,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    m.UserAgent,
+		ClientIp:     m.ClientIP,
 		IsBlocked:    false,
 		ExpiresAt:    refreshPayload.ExpiredAt,
 	})
